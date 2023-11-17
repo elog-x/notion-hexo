43e820483cd1324358ae2ea412f4700bc6928a5b
LetTTGACO <1225751694@qq.com>
2023-11-18 01:20:46 +0800

    feat: elog配置

--- format-image.js
+++ format-image.js
@@ -5,14 +5,17 @@ const { matterMarkdownAdapter } = require('@elog/cli')
  * 自定义文档处理器
  * @param {DocDetail} doc doc的类型定义为 DocDetail
  * @param {any} imageClient 图床下载器
- * @return {Promise<DocDetail>} 返回处理后的文档内容字符串
+ * @return {Promise<DocDetail>} 返回处理后的文档对象
  */
 const format = async (doc, imageClient) => {
   const cover = doc.properties.cover
   // 将 cover 字段中的 notion 图片下载到本地
-  const url = await imageClient.uploadImageFromUrl(cover, doc)
-  // cover链接替换为本地图片
-  doc.properties.cover = url
+  if (imageClient)  {
+    // 只有启用图床平台image.enable=true时，imageClient才能用，否则请自行实现图片上传
+    const url = await imageClient.uploadImageFromUrl(cover, doc)
+    // cover链接替换为本地图片
+    doc.properties.cover = url
+  }
   doc.body = matterMarkdownAdapter(doc);
   return doc;
 };
